472263b07101376a77b2635f63865c73b2312a2b
Kronen <bbulletb@gmail.com>
2022-12-02 06:18:38 -0500

    Starting to add a STATE object

--- src/CardList.js
+++ src/CardList.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Card from './Card'
-import { robots } from './robots'
 
-const CardList = ({ props }) => {
-    const cardTraverse = robots.map((user, i) => {
-        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} username={robots[i].username}/>
-    })
-    
-    return(
-        <div>
-            {cardTraverse}
-        </div>
+const CardList = ({ robots }) => {
+  const cardTraverse = robots.map((user, i) => {
+    return (
+      <Card
+        key={i}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+        username={robots[i].username}
+      />
     )
+  })
+
+  return <div>{cardTraverse}</div>
 }
 
 export default CardList
