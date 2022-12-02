f54095c06fd9cedb4e467716826656b8f199ea70
Kronen <bbulletb@gmail.com>
2022-12-02 05:06:22 -0500

    Search Box Implementation

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Card = (props) => {
-    return(
-        <div className='tc bg-light-green dib br3 pa 3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${props.id}`} />
-            <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
-                <p>{props.username}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className='tc bg-light-green dib br4 pa3 ma3 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${props.id}`} />
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
+        <p>{props.username}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Card
